d966b7c76d38f20df94154a3aac392469dfc4b4d
pdc <chenjipdc@gmail.com>
2020-08-07 10:53:06 +0800

    单例模式

--- src/main/java/top/chenjipdc/design/patterns/DP01_Singleton.java
+++ src/main/java/top/chenjipdc/design/patterns/DP01_Singleton.java
@@ -6,3 +6,108 @@ package top.chenjipdc.design.patterns;
  */
 public class DP01_Singleton {
 }
+
+/**
+ * 最简单单例，依靠类加载保证创建
+ */
+class Singleton1 {
+    int a = 1;
+    private static final Singleton1 INSTANCE = new Singleton1();
+
+    public static Singleton1 getInstance(){
+        return INSTANCE;
+    }
+}
+
+/**
+ * 线程安全有问题单例
+ */
+class Singleton2 {
+    int a = 1;
+    private static Singleton2 INSTANCE = null;
+    public static Singleton2 getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new Singleton2();
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 暴力的方法上锁，效率低
+ */
+class Singleton3 {
+    int a = 1;
+    private static Singleton3 INSTANCE = null;
+    public static synchronized Singleton3 getInstance(){
+        if (INSTANCE == null){
+            INSTANCE = new Singleton3();
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 粒度更细加锁，双重检查方式。
+ * 但是高并发下可能有半初始化对象问题，导致a拿到的是0。
+ */
+class Singleton4 {
+    int a = 1;
+    private static Singleton4 INSTANCE = null;
+    private static final Object LOCK = new Object();
+    public static Singleton4 getInstance(){
+        if (INSTANCE == null){
+            synchronized (LOCK){
+                if (INSTANCE == null){
+                    INSTANCE = new Singleton4();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 细粒度单例修复半初始化问题，堪称比较完美单例。
+ * 属性添加volatile关键字
+ */
+class Singleton5 {
+    int a = 1;
+    private static volatile Singleton5 INSTANCE = null;
+    private static final Object LOCK = new Object();
+    public static Singleton5 getInstance(){
+        if (INSTANCE == null){
+            synchronized (LOCK){
+                if (INSTANCE == null){
+                    INSTANCE = new Singleton5();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
+
+/**
+ * 通过枚举创建单例
+ */
+class Singleton6 {
+    int a = 1;
+    public static Singleton6 getInstance(){
+        return  ESingleton6.INSTANCE.getInstance();
+    }
+
+    enum ESingleton6 {
+        /**
+         * 创建单例
+         */
+        INSTANCE;
+
+        private final Singleton6 singleton6;
+        ESingleton6() {
+            this.singleton6 = new Singleton6();
+        }
+        private Singleton6 getInstance(){
+            return singleton6;
+        }
+    }
+}
---
+++ src/test/java/top/chenjipdc/design/patterns/DP01_TestSingleton.java
@@ -0,0 +1,139 @@
+package top.chenjipdc.design.patterns;
+
+import org.junit.Test;
+
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * @author chenjipdc@gmail.com
+ * @date 2020/8/7 10:31 上午
+ */
+public class DP01_TestSingleton {
+
+    /**
+     * 怎么测也不会出异常
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton1() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton1 instance = Singleton1.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+
+    /**
+     * 多测几次就可以测出异常
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton2() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton2 instance = Singleton2.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+
+    /**
+     * 怎么测也不会出异常
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton3() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton3 instance = Singleton3.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+
+    /**
+     * 模拟不出版初始化对象时a的值为0，概率极小。只能模拟创建的对象是一个单例对象。
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton4() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton4 instance = Singleton4.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+
+    /**
+     * 怎么测也不会出异常
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton5() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton5 instance = Singleton5.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+
+    /**
+     * 怎么测也不会出异常
+     *
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSingleton6() throws InterruptedException {
+        final Set<String> set = new ConcurrentSkipListSet<>();
+        for (int i = 0; i < 50; i++) {
+            new Thread(() -> {
+                Singleton6 instance = Singleton6.getInstance();
+                set.add(instance.toString());
+            }).start();
+        }
+        Thread.sleep(1000);
+        for (String s : set) {
+            System.out.println(s);
+        }
+        assert set.size() == 1;
+    }
+}
